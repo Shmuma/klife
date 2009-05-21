9cb8a2536b438523f9d209d449e7528c86882fef
Max Lapan <max.lapan@gmail.com>
2009-05-21 09:33:24 +0400

    version read function implemented

--- klife-proc.c
+++ klife-proc.c
@@ -1,6 +1,7 @@
 #include <linux/kernel.h>
 #include <linux/proc_fs.h>
 
+#include "klife.h"
 #include "klife-proc.h"
 
 static struct proc_dir_entry *root;
@@ -17,7 +18,8 @@ int proc_register ()
 	if (!root)
 		return 1;
 
-	version = create_proc_read_entry (KLIFE_PROC_VERSION, 0644, root, &proc_version_read, NULL);
+	version = create_proc_read_entry (KLIFE_PROC_VERSION, 0644, root, 
+					  &proc_version_read, NULL);
 
 	return 0;
 }
@@ -31,7 +33,27 @@ int proc_free ()
 }
 
 
-static int proc_version_read (char *page, char **start, off_t off, int count, int *eof, void *data)
+static int proc_calc_metrics (char *page, char **start, off_t off,
+			      int count, int *eof, int len)
 {
-	return 0;
+	if (len <= off+count)
+		*eof = 1;
+	*start = page + off;
+	len -= off;
+	if (len < 0)
+		len = 0;
+	if (len > count)
+		len = count;
+	return len;
+}
+
+
+static int proc_version_read (char *page, char **start, off_t off, 
+			      int count, int *eof, void *data)
+{
+	int len;
+
+	len = sprintf (page, "Kernel Life game simulator %d.%d\n", 
+		       KLIFE_VER_MAJOR, KLIFE_VER_MINOR);
+	return proc_calc_metrics (page, start, off, count, eof, len);
 }
---
+++ klife.h
@@ -0,0 +1,7 @@
+#ifndef __KLIFE_H__
+#define __KLIFE_H__
+
+#define KLIFE_VER_MAJOR 0
+#define KLIFE_VER_MINOR 1
+
+#endif
